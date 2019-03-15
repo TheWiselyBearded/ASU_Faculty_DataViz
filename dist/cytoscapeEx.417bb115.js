d1848083ccde51f7b2b927a5eca8e58fe64a85a5
Alireza Bahremand <abahrema@asu.edu>
2019-03-14 17:33:32 -0700

    Cytoscape layout capabilities for positioning nodes. Next step is to dynamically change the layout.

--- dist/cytoscapeEx.417bb115.js
+++ dist/cytoscapeEx.417bb115.js
@@ -77237,12 +77237,61 @@ var cy = cytoscape({
     name: 'grid',
     rows: 1
   }
-});
+}); // Layout
+// http://js.cytoscape.org/#layouts
+
+var options = {
+  name: 'circle',
+  fit: true,
+  // whether to fit the viewport to the graph
+  padding: 30,
+  // the padding on fit
+  boundingBox: undefined,
+  // constrain layout bounds; { x1, y1, x2, y2 } or { x1, y1, w, h }
+  avoidOverlap: true,
+  // prevents node overlap, may overflow boundingBox and radius if not enough space
+  nodeDimensionsIncludeLabels: false,
+  // Excludes the label when calculating node bounding boxes for the layout algorithm
+  spacingFactor: undefined,
+  // Applies a multiplicative factor (>0) to expand or compress the overall area that the nodes take up
+  radius: undefined,
+  // the radius of the circle
+  startAngle: 3 / 2 * Math.PI,
+  // where nodes start in radians
+  sweep: undefined,
+  // how many radians should be between the first and last node (defaults to full circle)
+  clockwise: true,
+  // whether the layout should go clockwise (true) or counterclockwise/anticlockwise (false)
+  sort: undefined,
+  // a sorting function to order the nodes; e.g. function(a, b){ return a.data('weight') - b.data('weight') }
+  animate: false,
+  // whether to transition the node positions
+  animationDuration: 500,
+  // duration of animation in ms if enabled
+  animationEasing: undefined,
+  // easing of animation if enabled
+  animateFilter: function animateFilter(node, i) {
+    return true;
+  },
+  // a function that determines whether the node should be animated.  All nodes animated by default on animate enabled.  Non-animated nodes are positioned immediately when the layout starts
+  ready: undefined,
+  // callback on layoutready
+  stop: undefined,
+  // callback on layoutstop
+  transform: function transform(node, position) {
+    return position;
+  } // transform a given node position. Useful for changing flow direction in discrete layouts 
+
+};
+cy.layout(options); //   layout.start()
+// var layout = cy.layout({ name: 'random' });
+
+var layout = cy.layout(options);
+layout.run();
 var nodes = cy.nodes();
 nodes.forEach(function (n) {// console.log("Node", n.data());
   // console.log("Indegrees:\t" + n.indegree(false));
 }); // console.log(nodes);
-// console.log("lol");
 },{"cytoscape":"node_modules/cytoscape/dist/cytoscape.cjs.js","get-json":"node_modules/get-json/index.js","csvtojson":"node_modules/csvtojson/browser/browser.js","jquery":"node_modules/jquery/dist/jquery.js","./nLinks.json":"nLinks.json"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -77270,7 +77319,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58494" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50257" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
